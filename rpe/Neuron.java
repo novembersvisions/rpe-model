d6807406aab3ca2a283655f06720c11cce65f529
Hannah Price <89949050+novembersvisions@users.noreply.github.com>
2023-03-06 14:51:59 -0500

    outline new methods, add specs

--- rpe/Neuron.java
+++ rpe/Neuron.java
@@ -29,13 +29,62 @@ public class Neuron {
     }
 
     /** Creates an instance of Neuron.
-     @param type: specifies the type of neuron; must be "excite" or "inhibit"
-     @param next: list of neurons this object is connected to
+     @param type specifies the type of neuron; must be "excite" or "inhibit"
+     @param next list of neurons this object is connected to
      */
     Neuron(String type, List<Neuron> next) {
         this.type = type;
         this.next = next;
+        potential = 70.0;
         assertInv();
+    }
+
+    /**
+     * Returns the list of connected neurons in `next`
+     */
+    public List<Neuron> next() {
+        return next;
+    }
+
+    /**
+     * Adds a new neuron, `n`, to field `next`.
+     * @param n new neuron to add
+     */
+    public void ltp(Neuron n) {
+        next.add(n);
+        assertInv();
+    }
+
+    /**
+     * Removes all neurons from the field `next`.
+     */
+    public void ltdAll() {
+        int i = 0;
+        while (i < next.size()) {
+            next.remove(next.get(i));
+        }
+        assertInv();
+    }
+
+    /**
+     * Calculates membrane potential in mV using the Goldman–Hodgkin–Katz equation.
+     * Modifies the value of field `potential`.
+     */
+    public void setPotential() {
+
+    }
+
+    /** Allows the neuron to fire, returning true if the membrane potential has reached the threshold.
+     * If the neuron is excitatory, it depolarizes the connected neurons in `next`,
+     * setting membrane potential accordingly.
+     * If the neuron is inhibitory, it repolarizes the membrane potential of connected neurons. */
+    public boolean fire() {
+        if (potential < -55.0) {
+            return false;
+        }
+        else {
+            return true;
+        }
     }
 
 }
