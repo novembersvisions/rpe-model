81f29ce6e69dbbfa6d80166cc44766a60ffddd3c
Hannah Price <89949050+novembersvisions@users.noreply.github.com>
2023-03-06 14:20:10 -0500

    testing neuron constructor

--- rpe/Neuron.java
+++ rpe/Neuron.java
@@ -6,8 +6,8 @@ import java.util.List;
 public class Neuron {
 
     /** A list of neurons this neuron is connected to.
-     * Invariant: a neuron cannot be connected to itself or null */
-    private List<Neuron> next = new ArrayList<Neuron>();
+     * Invariant: a neuron cannot be connected to itself */
+    public List<Neuron> next = new ArrayList<Neuron>();
 
     /** Stores whether the neuron is excitatory or inhibitory.
      * Invariant: `type` must either be "excite" or "inhibit" */
@@ -19,19 +19,23 @@ public class Neuron {
     /** Asserts class invariants. */
     private void assertInv() {
         int i = 0;
-        while (i < next.size()) {
-            assert next.get(i) != null && next.get(i) != this;
+        if (next != null) {
+            while (i < next.size()) {
+                assert next.get(i) != this;
+                i += 1;
+            }
         }
         assert type == "excite" || type == "inhibit";
     }
 
     /** Creates an instance of Neuron.
-     Parameter `type`: specifies the type of neuron; "excite" or "inhibit"
-     Parameter `next`: list of neurons this object is connected to
+     @param type: specifies the type of neuron; must be "excite" or "inhibit"
+     @param next: list of neurons this object is connected to
      */
     Neuron(String type, List<Neuron> next) {
         this.type = type;
         this.next = next;
+        assertInv();
     }
 
 }
--- rpe/NeuronTest.java
+++ rpe/NeuronTest.java
@@ -1,5 +1,8 @@
 package rpe;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,5 +14,12 @@ class NeuronTest {
      */
     @Test
     void testConstructor() {
+        Neuron n = new Neuron("excite",null);
+        ArrayList<Neuron> iList = new ArrayList<Neuron>(Arrays.asList(n));
+
+        Neuron i = new Neuron("inhibit", iList);
+        System.out.println(i);
+        System.out.println(n);
+        System.out.println(i.next);
     }
 }
